b9257362c5e8adc263a79192e7e2d90ea29a63bc
krschroeder <kevin.r.schroeder@gmail.com>
2023-12-19 16:26:25 -0500

    Smooth scroll fix for IOS devices, so it will actually scroll to the element on those devices

--- src/assets/js/util/smooth-scroll.ts
+++ src/assets/js/util/smooth-scroll.ts
@@ -2,29 +2,41 @@ import $ from 'cash-dom';
 
 const docTop = () => document.documentElement.scrollTop || document.body.scrollTop || 0;
 
+// We need to throttle the checking of the previous scroll for a bug in IOS
+// that says the previous pixel is the same as the current pixel.
+// Q: Why do we need to check if the current pixel is the same as the previous?
+// A: Because it could indicate that the element cannot be completely scrolled to
+//    and as a result we need to break this JS scroll 
+const checkIterationAmt = 3;
+
 let activeScroll = false;
 
 export default function smoothScroll(
-    elemYPos: number, 
-    _speed = 100, 
-    afterScroll?:(...args) => void, 
+    elemYPos: number,
+    _speed = 100,
+    afterScroll?: (...args) => void,
     afterScrollArgs: any[] = []
-):void {
-    
+): void {
+
     // If an active scroll is going exit until it's done
     if (activeScroll) return;
 
-    activeScroll = true;
-
-    let prevScroll = null;
     const speed = _speed / 1000;
-    let animation = null;
-    let userBreakScroll = false;
+    
+    activeScroll = true;
+    let 
+        prevScroll = null,
+        animation = null,
+        userBreakScroll = false,
+        pxToCheckIteration = 0
+    ;
 
     const targetIsAbove = elemYPos < docTop();
-    
-    $(window).on('wheel.smoothScroll',()=> {userBreakScroll = true})
-   
+
+    $(window).on('wheel.smoothScroll', () => { userBreakScroll = true });
+
+    document.body.style.scrollBehavior = 'auto';
+
     const scrollDone = () => {
         if (typeof afterScroll === 'function') {
             afterScroll.apply(null, afterScrollArgs);
@@ -32,6 +44,7 @@ export default function smoothScroll(
         window.cancelAnimationFrame(animation);
         activeScroll = false;
         $(window).off('wheel.smoothScroll');
+        document.body.style.scrollBehavior = null;
     }
 
     (function smoothScrollInner() {
@@ -42,16 +55,20 @@ export default function smoothScroll(
             return;
         }
 
-        prevScroll = currentScroll;
+        if (pxToCheckIteration === checkIterationAmt) {
+            prevScroll = currentScroll;
+            pxToCheckIteration = 0;
+
+        } else {
+            pxToCheckIteration++;
+        }
 
         const isAtTarget = Math.floor(currentScroll - elemYPos) === 0;
         const isPastTarget = targetIsAbove ? prevScroll < currentScroll : prevScroll > currentScroll;
-        
 
         if (!isAtTarget || !isPastTarget) {
 
             animation = window.requestAnimationFrame(smoothScrollInner);
-
             window.scroll(0, currentScroll + ((elemYPos - currentScroll) * speed));
         } else {
             scrollDone();
